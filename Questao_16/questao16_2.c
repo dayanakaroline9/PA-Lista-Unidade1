5026ef735cd93f54383c10b7a30c1a7b96089556
dayanakaroline9 <dayanakaroline9@gmail.com>
2022-09-23 23:43:51 -0300

    Create questao16_2.c

---
+++ Questao_16/questao16_2.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <time.h>      
+#include <stdlib.h>
+
+//função troca
+//recebe dois ponteiros para float e troca os valores
+void troca (float *a, float *b){
+  float aux = *a;
+  *a = *b;
+  *b = aux;
+}
+
+//função ordena
+//recebe um array alocado de forma dinamica e o tamanho do array
+//percorre o array e compara qual menor valor entre dois e troca , ordenando o array de forma crescente
+void ordena (float *x, int n){
+  for(int i=n-1;i>=1;i--){
+    for(int j=0;j<i;j++){
+      //acessa o valor caminhando na memoria
+      if(*(x+i)<*(x+j)){
+        //troca recebe o endereço do valor acessado
+        troca(&*(x+i),&*(x+j));
+      }
+    }
+  }
+}
+
+
+// função principal para encontrar o tempo de execução de um programa em C
+int main()
+{
+    // para armazenar o tempo de execução do código
+    double tempo = 0.0;
+ 
+    clock_t inicio = clock();
+ 
+    //executa o codigo de ordenação
+    //recebendo o tamanho do vetor
+  int n;
+  printf("Digite quantos numeros serão ordenados:\n");
+  scanf("%d",&n);
+  //criando o vetor como ponteiro
+  float *x;
+
+  //alocando vetor
+  x = malloc(n*sizeof(float));
+
+  //preenchendo vetor
+  for(int i=0; i<n; i++){
+    scanf("%f",&x[i]);
+  }
+
+  //mostrando vetor antes da aordenação
+  printf("\n");
+  for(int i=0; i<n; i++){
+    printf("%f ",x[i]);
+  }
+
+  //chamando a função ordena, passando o vetor e o tamanho
+  ordena (x, n);
+
+  //mostrando vetor ordenado
+  printf("\n");
+  for(int i=0; i<n; i++){
+    printf("%f ",x[i]);
+  }
+
+  //liberando memoria alocada
+  free(x);
+
+
+  
+    clock_t fim = clock();
+ 
+    // calcula o tempo decorrido encontrando a diferença (final - inicio) e
+    // dividindo a diferença por CLOCKS_PER_SEC para converter em segundos
+    tempo += (double)(fim - inicio) / CLOCKS_PER_SEC;
+ 
+    printf("\nO tempo é %f segundos", tempo);
+ 
+    return 0;
+}
